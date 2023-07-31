c2ced928cfb4c35257fa9654c0d2352619aeaa33
Diego Saavedra <dsaavedra88@gmail.com>
2023-07-31 12:15:19 -0500

    ventas

--- ventas/admin.py
+++ ventas/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Cliente, Producto, Pedido
+from .models import Cliente, Producto, Venta
 
 admin.site.register(Cliente)
 admin.site.register(Producto)
-admin.site.register(Pedido)
+admin.site.register(Venta)
--- ventas/models.py
+++ ventas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Cliente(models.Model):
-    cédula = models.IntegerField(primary_key=True)
+    documento_detalle = models.IntegerField(verbose_name='Número de documento', unique=True)
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     dirección = models.CharField(max_length=100)
@@ -10,7 +10,7 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nombre + ' ' + self.apellido
-    
+
 class Producto(models.Model):
     código = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=50)
@@ -19,14 +19,22 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.nombre
-    
-class Pedido(models.Model):
+
+class Venta(models.Model):
     número = models.IntegerField(primary_key=True)
     fecha = models.DateField(auto_now=True)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad = models.IntegerField()
+    observaciones = models.CharField(max_length=50, verbose_name='Observaciones de la venta')
+    iva = models.FloatField(choices=[(0.12, '12%')])
+    descuento = models.FloatField(choices=[(0.1, '10%'), (0.2, '20%'), (0.3, '30%')])
+
+    @property
+    def total_calculado(self):
+        subtotal = self.producto.precio * self.cantidad
+        total = subtotal + (subtotal * self.iva) - (subtotal * self.descuento)
+        return total
 
     def __str__(self):
-        return str(self.número)
-    
+        return f"{self.cliente} - {self.producto}, ha adquirido una {self.producto.nombre} por el costo total de: {self.total_calculado}"
